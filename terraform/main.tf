458c960f16ad90d93e8e1e42c65726862bda45cc
JaanLavaerts <jaan.lavaerts@solita.fi>
2025-11-13 10:34:55 +0100

    tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,78 @@
 resource "azurerm_resource_group" "jaan-cloud-resume" {
   name     = "jaan-cloud-resume"
   location = "West Europe"
+  tags     = { "Owner" = "Jaan", "DueDate" = "2025-12-31" }
 }
 
-resource "azurerm_static_web_app" "frontend" {
-  name                = "frontend"
+resource "azurerm_static_web_app" "frontend-resume-jaan" {
+  name                = "frontend-resume-jaan"
   resource_group_name = azurerm_resource_group.jaan-cloud-resume.name
   location            = azurerm_resource_group.jaan-cloud-resume.location
 }
 
+resource "azurerm_storage_account" "st-resume-jaan" {
+  name                     = "stresumejaan"
+  resource_group_name      = azurerm_resource_group.jaan-cloud-resume.name
+  location                 = azurerm_resource_group.jaan-cloud-resume.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
+resource "azurerm_service_plan" "service-plan-jaan" {
+  name                = "service-plan-jaan"
+  location            = azurerm_resource_group.jaan-cloud-resume.location
+  resource_group_name = azurerm_resource_group.jaan-cloud-resume.name
+  sku_name            = "B1"
+  os_type             = "Linux"
+}
+
+resource "azurerm_linux_function_app" "func-resume-jaan" {
+  name                       = "func-resume-jaan"
+  location                   = azurerm_resource_group.jaan-cloud-resume.location
+  resource_group_name        = azurerm_resource_group.jaan-cloud-resume.name
+  service_plan_id            = azurerm_service_plan.service-plan-jaan.id
+  storage_account_name       = azurerm_storage_account.st-resume-jaan.name
+  storage_account_access_key = azurerm_storage_account.st-resume-jaan.primary_access_key
+
+  site_config {}
+}
+
+resource "azurerm_cosmosdb_account" "cosmos-resume-jaan" {
+  name                = "cosmos-resume-jaan"
+  location            = azurerm_resource_group.jaan-cloud-resume.location
+  resource_group_name = azurerm_resource_group.jaan-cloud-resume.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+  consistency_policy {
+    consistency_level = "Session"
+  }
+  geo_location {
+    location          = azurerm_resource_group.jaan-cloud-resume.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "visitors-database" {
+  name                = "visitors-database"
+  resource_group_name = azurerm_resource_group.jaan-cloud-resume.name
+  account_name        = azurerm_cosmosdb_account.cosmos-resume-jaan.name
+}
+
+resource "azurerm_cosmosdb_sql_container" "visitors-container" {
+  name                = "visitors-container"
+  resource_group_name = azurerm_resource_group.jaan-cloud-resume.name
+  account_name        = azurerm_cosmosdb_account.cosmos-resume-jaan.name
+  database_name       = azurerm_cosmosdb_sql_database.visitors-database.name
+  throughput          = 400
+  partition_key_paths = ["/id"]
+}
 
 # outputs
 output "static_web_app_url" {
-  value = azurerm_static_web_app.frontend.default_host_name
+  value = azurerm_static_web_app.frontend-resume-jaan.default_host_name
 }
 
 output "deployment_token" {
-  value     = azurerm_static_web_app.frontend.api_key
+  value     = azurerm_static_web_app.frontend-resume-jaan.api_key
   sensitive = true
 }
